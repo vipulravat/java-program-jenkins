c78d3d25cf3789e5399b590f247930a292b53741
Vipul Ravat <85198025+vipulravat@users.noreply.github.com>
2024-01-15 13:12:45 +0100

    initial commit

--- Main.java
+++ Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("Hope you this email find you well");
         student student=new student();
         student.sayHello();
+
     }
     }
---
+++ student.java
@@ -0,0 +1,11 @@
+public class student {
+
+    public student(){
+        System.out.println("creating student object");
+    }
+
+     public void sayHello() {
+
+        System.out.println("Hii , I'm vipul");
+     }
+}
