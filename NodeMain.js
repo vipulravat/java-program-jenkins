a117e2cb8aaddfbb2cbb5be713e090ebe63b743b
Vipul Ravat <85198025+vipulravat@users.noreply.github.com>
2024-01-15 15:54:29 +0100

    initial commit

--- NodeMain.js
+++ NodeMain.js
@@ -1,5 +1,3 @@
 console.log("This is node file")
 console.log("I'm going to run on jenkins")
-console.log("added new comment")
-console.log("added one more comment")
-
+console.log("new commmand add")
