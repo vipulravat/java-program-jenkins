94f0629c60c9eaccbeffaa2a3660e91b9529d79b
Vipul Ravat <85198025+vipulravat@users.noreply.github.com>
2024-01-15 12:19:20 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is java program");
+        System.out.println("output from the java program");
+        System.out.println(new Date());
+        System.out.println("hello");
+        System.out.println("Hope you this email find you well");
+    }
+    }
